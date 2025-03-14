1dc05ff210649aed5c2869704c5de262ee7b4797
LeonhartAnnie <122689484+LeonhartAnnie@users.noreply.github.com>
2025-03-14 10:45:43 +0800

    random
    random your random

--- lotto_analyze.py
+++ lotto_analyze.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import random
 from collections import Counter
 import heapq
 data_path = os.path.join(os.path.expanduser("~"),"Downloads","data","data.csv")
@@ -12,4 +13,14 @@ with open(data_path,newline='',encoding="utf-8") as csvfile:
         for num in row:
             lotto_nums.append(int(num))
 lotto_nums.sort()
+a=[]
+for i in range(0,7):
+    b=random.randint(0,len(lotto_nums)-1)
+    a.append(lotto_nums[b])
+    for num in lotto_nums:
+        if num==a:
+            lotto_nums.remove(num)
+            break
+a.sort()
+print(a)
 print(lotto_nums)
